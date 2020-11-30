e13cc4abb8034c5f6b4eb96d96824ba4701adddc
bkammerling <bkammerling@googlemail.com>
2020-11-30 10:27:05 +0000

    remove post pull with slug and date

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Layout from '../components/layout'
 import Container from 'react-bootstrap/Container';
 import styles from '../components/index.module.scss'
-import { fetchEntries } from '../lib/contentfulPosts'
-
-//import { getSortedPostsData } from '../lib/posts'
 
 export default class Home extends Component {
   constructor(props){
@@ -51,47 +48,8 @@ export default class Home extends Component {
               </div>
           </Container>
         </div>
-
-        {/*
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Blog</h2>
-          <ul className={utilStyles.list}>
-            {posts.map((p) => (
-              <li className={utilStyles.listItem} key={p.slug}>
-                <Link href={`/posts/${p.slug}`}>
-                  <a>{p.title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  <Date dateString={p.date} />
-                </small>
-              </li>
-            ))}
-          </ul>
-          <div className="posts">
-            {posts.map((p) => {
-              return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
-            })}
-          </div>
-        </section>
-          */}
-
       </Layout>
     )
   }
+  
 }
-
-export async function getStaticProps() {
-  const res = await fetchEntries()
-  const posts = await res.map((p) => {
-    return p.fields
-  })
-  //console.log(posts);
-  return {
-    props: {
-      posts,
-    },
-  }
-}
-
-
